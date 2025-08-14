ab7560ec0b9cf59beb2d24af0bd1d326b7e45399
Adriano França Monteiro <adriano.franca@discente.ufma.br>
2025-08-14 13:25:23 -0300

    alteracoes para usuarios não conseguirem editar informações do outro

--- src/main/java/br/com/bookcheck/controller/UsuarioLeitorController.java
+++ src/main/java/br/com/bookcheck/controller/UsuarioLeitorController.java
@@ -1,9 +1,10 @@
 package br.com.bookcheck.controller;
 
-import br.com.bookcheck.controller.dto.request.Usuario.UsuarioLeitorRequestDto;
 import br.com.bookcheck.controller.dto.request.Usuario.UsuarioLeitorUpdateRequestDto;
 import br.com.bookcheck.controller.dto.response.GenericSuccessResponseDto;
 import br.com.bookcheck.controller.dto.response.Usuario.UsuarioLeitorResponseDto;
+import br.com.bookcheck.domain.entity.Usuario.Usuario;
+import br.com.bookcheck.domain.repository.UsuarioRepository;
 import br.com.bookcheck.service.Usuario.UsuarioService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -14,10 +15,12 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
 
 @Controller
 @RequestMapping("api/leitor")
@@ -26,12 +29,14 @@ import org.springframework.web.bind.annotation.*;
 public class UsuarioLeitorController {
 
     private final UsuarioService usuarioService;
+    private final UsuarioRepository usuarioRepository;
 
     @Operation(summary = "Editar leitor", description = "Edita usuário do tipo leitor no sistema")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "Leitor editado com sucesso",
+            @ApiResponse(responseCode = "200", description = "Leitor editado com sucesso",
                     content = @Content(schema = @Schema(implementation = UsuarioLeitorResponseDto.class))),
             @ApiResponse(responseCode = "400", description = "Dados inválidos fornecidos"),
+            @ApiResponse(responseCode = "403", description = "Acesso negado"),
             @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
     })
     @PutMapping("/{id}")
@@ -39,10 +44,18 @@ public class UsuarioLeitorController {
             @Parameter(description = "Id do leitor a ser editado", required = true)
             @Valid @PathVariable Long id,
             @Parameter(description = "Dados do leitor a ser editado", required = true)
-            @Valid @RequestBody UsuarioLeitorUpdateRequestDto request) {
+            @Valid @RequestBody UsuarioLeitorUpdateRequestDto request,
+            Principal principal) {
 
-        UsuarioLeitorResponseDto response = usuarioService.updateLeitor(id,request);
+        String userEmail = principal.getName();
+        Usuario usuarioLogado = usuarioRepository.findByEmail(userEmail)
+                .orElseThrow(() -> new AccessDeniedException("Usuário não encontrado."));
+
+        if (!usuarioLogado.getId().equals(id)) {
+            throw new AccessDeniedException("Um usuário não pode editar o perfil de outro.");
+        }
+
+        UsuarioLeitorResponseDto response = usuarioService.updateLeitor(id, request);
         return ResponseEntity.ok(new GenericSuccessResponseDto<>("Leitor atualizado com sucesso.", "usuarioLeitor", response));
     }
-
 }
--- src/main/java/br/com/bookcheck/controller/UsuarioSeboController.java
+++ src/main/java/br/com/bookcheck/controller/UsuarioSeboController.java
@@ -3,6 +3,8 @@ package br.com.bookcheck.controller;
 import br.com.bookcheck.controller.dto.request.Usuario.UsuarioSeboRequestDto;
 import br.com.bookcheck.controller.dto.response.GenericSuccessResponseDto;
 import br.com.bookcheck.controller.dto.response.Usuario.UsuarioSeboResponseDto;
+import br.com.bookcheck.domain.entity.Usuario.Usuario;
+import br.com.bookcheck.domain.repository.UsuarioRepository;
 import br.com.bookcheck.service.Usuario.UsuarioService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -13,13 +15,15 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Controller
 @RequestMapping("api/sebo")
@@ -28,12 +32,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UsuarioSeboController {
 
     private final UsuarioService usuarioService;
+    private final UsuarioRepository usuarioRepository;
 
     @Operation(summary = "Editar Sebo", description = "Edita usuário do tipo Sebo no sistema")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "Sebo editado com sucesso",
+            @ApiResponse(responseCode = "200", description = "Sebo editado com sucesso",
                     content = @Content(schema = @Schema(implementation = UsuarioSeboResponseDto.class))),
             @ApiResponse(responseCode = "400", description = "Dados inválidos fornecidos"),
+            @ApiResponse(responseCode = "403", description = "Acesso negado"),
             @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
     })
     @PutMapping("/{id}")
@@ -41,10 +47,19 @@ public class UsuarioSeboController {
             @Parameter(description = "Id do Sebo a ser editado", required = true)
             @Valid @PathVariable Long id,
             @Parameter(description = "Dados do Sebo a ser editado", required = true)
-            @Valid @RequestBody UsuarioSeboRequestDto request) {
+            @Valid @RequestBody UsuarioSeboRequestDto request,
+            Principal principal) {
 
-        UsuarioSeboResponseDto response = usuarioService.createSebo(request);
+        String userEmail = principal.getName();
+        Usuario usuarioLogado = usuarioRepository.findByEmail(userEmail)
+                .orElseThrow(() -> new AccessDeniedException("Usuário não encontrado."));
+
+
+        if (!usuarioLogado.getId().equals(id)) {
+            throw new AccessDeniedException("Um usuário não pode editar o perfil de outro.");
+        }
+
+        UsuarioSeboResponseDto response = usuarioService.updateSebo(id, request);
         return ResponseEntity.ok(new GenericSuccessResponseDto<>("Sebo atualizado com sucesso.", "usuarioSebo", response));
     }
-
 }
